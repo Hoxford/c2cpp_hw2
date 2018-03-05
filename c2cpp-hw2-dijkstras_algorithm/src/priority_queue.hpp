d8d5d1ea875acd17f19e9340476b0bf313221b4f
storm <storm@10.0.0.137>
2018-03-04 23:21:08 -0800

    for turn in 2

--- c2cpp-hw2-dijkstras_algorithm/src/priority_queue.hpp
+++ c2cpp-hw2-dijkstras_algorithm/src/priority_queue.hpp
@@ -14,57 +14,7 @@
 
 using namespace std;
 
-class Shortest_Path
-{
-public:
-    //the constructor will determine the shortest path between start and end
-    Shortest_Path(Graph * graph, unsigned int start, unsigned int end);
-    ~Shortest_Path();
-    //returns the shortest path between start and end
-    list<unsigned int> *path();
-    //return the path cost associated with the shortest path.
-    unsigned int path_size(int u, int w);
-private:
-    unsigned int path_exists(unsigned int node_a, unsigned int node_b);
-    Priority_Queue<unsigned int, float> pQueue;
-    list<unsigned int> shortest_path_;
-    unsigned int start_;
-    unsigned int end_;
-    Graph * graph_;
-};
 
-Shortest_Path::Shortest_Path(Graph * graph, unsigned int start, unsigned int end)
-{
-//    shortest_path_ = NULL;
-    start_ = start;
-    end_ = end;
-    graph_ = graph;
-
-}
-
-Shortest_Path::~Shortest_Path()
-{
-
-}
-
-inline list<unsigned int> *Shortest_Path::path()
-{
-    for(unsigned int i = 0; i < graph_->total_vertices_; i++)
-    {
-        if(graph_->)
-    }
-    return &shortest_path_;
-}
-
-unsigned int Shortest_Path::path_size(int u, int w)
-{
-    return 0;
-}
-
-unsigned int Shortest_Path::path_exists(unsigned int node_a, unsigned int node_b)
-{
-
-}
 
 template <typename T1, typename T2>
 class Priority_Queue
